a92f6319512c18c8f666c5e5a0839ffbe251e780
Walkers15 <qortmd7777@gmail.com>
2023-01-02 10:54:53 +0900

    update: gpt max token 값 변경

--- src/routes/gpt.ts
+++ src/routes/gpt.ts
@@ -3,28 +3,28 @@ import type { AxiosInstance } from "axios";
 import axios from "axios";
 
 export interface GptData {
-    id: string;
-    generations: Generation[];
+  id: string;
+  generations: Generation[];
 }
-  
+
 export interface Generation {
-    text: string;
-    tokens: number;
+  text: string;
+  tokens: number;
 }
 
 export const gptRouter = Router();
 
 gptRouter.post("/", async (req, res) => {
-    const kakaoAxios: AxiosInstance = axios.create();
-    kakaoAxios.defaults.headers.common.Authorization = `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_KEY}`;
+  const kakaoAxios: AxiosInstance = axios.create();
+  kakaoAxios.defaults.headers.common.Authorization = `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_KEY}`;
 
-    const getGptData = async (prompt: string): Promise<GptData> => {
-        let response: GptData = null;
-        await kakaoAxios
-            .post<GptData>("https://api.kakaobrain.com/v1/inference/kogpt/generation", { prompt, max_tokens: 1 })
-            .then((res) => response = res.data);
-        return response;
-    };
+  const getGptData = async (prompt: string): Promise<GptData> => {
+    let response: GptData = null;
+    await kakaoAxios
+      .post<GptData>("https://api.kakaobrain.com/v1/inference/kogpt/generation", { prompt, max_tokens: 128 })
+      .then((res) => (response = res.data));
+    return response;
+  };
 
-    return res.json(await getGptData(req.body.prompt));
+  return res.json(await getGptData(req.body.prompt));
 });
